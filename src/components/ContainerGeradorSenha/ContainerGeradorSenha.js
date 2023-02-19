fe41b2f922e8c457b394632d76bbc9645549cab1
Anderson Babinski <andeerlb@gmail.com>
2023-02-18 22:16:56 -0300

    criado title

--- src/components/ContainerGeradorSenha/ContainerGeradorSenha.js
+++ src/components/ContainerGeradorSenha/ContainerGeradorSenha.js
@@ -5,6 +5,9 @@ import styles from "./ContainerGeradorSenha.module.css";
 const ContainerGeradorSenha = () => {
     return (
         <div id={styles.container}>
+            <span className={styles.title}>
+                Password Generator
+            </span>
             <InputPassword />
             <GeradorSenha />
         </div>
