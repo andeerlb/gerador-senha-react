c9688b008f5421b33cfac90dace5f49dea3e5c12
Anderson Babinski <andeerlb@gmail.com>
2023-02-19 16:32:57 -0300

    desabilitado select

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
   }
 
   return (
-    <div className="App">
+    <div className="App noselect">
       <InputPasswordContext.Provider value={{
         inputPasswordState,
         changePassword
