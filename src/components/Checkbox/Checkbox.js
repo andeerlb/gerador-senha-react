3463e793381ee46ef559f297ae67f7d7dcc72e5a
Anderson Babinski <andeerlb@gmail.com>
2023-02-19 15:54:40 -0300

    criada implementação das ações dos componentes

--- src/components/Checkbox/Checkbox.js
+++ src/components/Checkbox/Checkbox.js
@@ -1,9 +1,9 @@
 import styles from "./Checkbox.module.css";
 
-const Checkbox = ({ id=new Date().getTime(), label="Descricao checkbox" }) => {
+const Checkbox = ({ id=new Date().getTime(), label="Descricao checkbox", onChange = (isChecked) => {} }) => {
     return (
         <div className={styles.containerCheckbox}>
-            <input id={id} type="checkbox" />
+            <input id={id} type="checkbox" onChange={e => onChange(e.target.checked)}/>
             <label htmlFor={id}>{label}</label>
         </div>
     )
--- src/components/GeradorSenha/GeradorSenha.js
+++ src/components/GeradorSenha/GeradorSenha.js
@@ -3,22 +3,25 @@ import Checkbox from "../Checkbox/Checkbox";
 import Button from "../Button/Button";
 import { useContext, useState } from "react";
 import { InputPasswordContext } from "../../context/InputPasswordContext";
+import { GeradorSenhaContext } from "../../context/GeradorSenhaContext";
+
+const CHARACTER_LENGTH_MIN = 1;
+const CHARACTER_LENGTH_MAX = 100;
 
 const CharacterLength = () => {
-    const [rangeValue, setRangeValue] = useState(0)
+    const geradorSenhaContext = useContext(GeradorSenhaContext);
 
     const handleRangeValue = (e) =>{
-        setRangeValue(e.target.value);
+        geradorSenhaContext.setLength(e.target.value);
     }
-    
     
     return (
         <div className={styles.characterLengthContainer}>
             <div>
                 <span>Character Length</span>
-                <span className={styles['range-value']}> <p>{rangeValue}</p> </span>
+                <span className={styles['range-value']}> <p>{geradorSenhaContext.geradorSenha.length}</p> </span>
             </div>
-            <input type="range" name="rangeInput" value={rangeValue}  min="0" max="50" onChange={handleRangeValue} />
+            <input type="range" name="rangeInput" value={geradorSenhaContext.geradorSenha.length}  min={CHARACTER_LENGTH_MIN} max={CHARACTER_LENGTH_MAX} onChange={handleRangeValue} />
         </div>
     )
 }
@@ -37,39 +40,124 @@ const ForcaDaSenha = () => {
     )
 }
 
+const gerarListaDeChars = (geradorSenha) => {
+    const CHARS_NUMERICOS = "0123456789";
+    const CHARS_MINUSCULO = "abcdefghijklmnopqrstuvwxyz";
+    const CHARS_MAIUSCULO = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const CHARS_SIMBOLOS = "!@#$%^&*()";
+
+    let chars = "";
+    if(geradorSenha.hasNumbers) {
+        chars += CHARS_NUMERICOS;
+    }
+
+    if(geradorSenha.hasSymbols) {
+        chars += CHARS_SIMBOLOS;
+    }
+
+    if(geradorSenha.isLowercase) {
+        chars += CHARS_MINUSCULO;
+    }
+
+    if(geradorSenha.isUppercase) {
+        chars += CHARS_MAIUSCULO;
+    }
+
+    if(!chars.length) {
+        return CHARS_MINUSCULO;
+    }
+
+    return chars;
+}
+
 const GeradorSenha = () => {
     const inputPasswordContext = useContext(InputPasswordContext);
+    const [geradorSenha, setGeradorSenha] = useState({
+        length: CHARACTER_LENGTH_MIN,
+        isUppercase: false,
+        isLowercase: false,
+        hasNumbers: false,
+        hasSymbols: false
+    });
+
+    const setLength = (length = CHARACTER_LENGTH_MAX) => {
+        setGeradorSenha({
+            ...geradorSenha,
+            length
+        })
+    }
+
+    const setUppercase = (uppercase = false) => {
+        setGeradorSenha({
+            ...geradorSenha,
+            isUppercase: uppercase
+        })
+    }
+
+    const setLowercase = (lowercase = false) => {
+        setGeradorSenha({
+            ...geradorSenha,
+            isLowercase: lowercase
+        })
+    }
+
+    const setHasNumbers = (hasNumbers = false) => {
+        setGeradorSenha({
+            ...geradorSenha,
+            hasNumbers
+        })
+    }
+
+    const setHasSymbols = (hasSymbols = false) => {
+        setGeradorSenha({
+            ...geradorSenha,
+            hasSymbols
+        })
+    }
 
     const gerarSenha = () => {
-        console.log('aqui vai gerar a senha');
-        inputPasswordContext.changePassword("nova senha, time: "+new Date().getTime());
-        console.log(inputPasswordContext);
+        const CHARS = gerarListaDeChars(geradorSenha);
+        let password = "";
+        for (var index = 0; index <= geradorSenha.length-1; index++) {
+            const numeroAleatorio = Math.floor(Math.random() * CHARS.length);
+            password += CHARS.substring(numeroAleatorio, numeroAleatorio+1);
+        }
+        inputPasswordContext.changePassword(password);
     }
 
     return (
-        <div className={styles.containerGerador}>
-            <div className="m-b-10">
-                <CharacterLength />
+        <GeradorSenhaContext.Provider value={{
+            geradorSenha,
+            setLength,
+            setUppercase,
+            setLowercase,
+            setHasNumbers,
+            setHasSymbols
+        }}>
+            <div className={styles.containerGerador}>
+                <div className="m-b-10">
+                    <CharacterLength />
+                </div>
+                <div className="m-b-15">
+                    <Checkbox id="uppercase" label="Include Uppercase Letters" onChange={isChecked => setUppercase(isChecked)}/>
+                </div>
+                <div className="m-b-15">
+                    <Checkbox id="lowercase" label="Include Lowercase Letters" onChange={setLowercase}/>
+                </div>
+                <div className="m-b-15">
+                    <Checkbox id="numbers" label="Include Numbers" onChange={setHasNumbers}/>
+                </div>
+                <div className="m-b-15">
+                    <Checkbox id="symbols" label="Include Symbols" onChange={setHasSymbols}/>
+                </div>
+                <div className="m-t-15 m-b-15">
+                    <ForcaDaSenha />
+                </div>
+                <div className="m-b-15">
+                    <Button  text="Generate" onClick={gerarSenha}/>
+                </div>
             </div>
-            <div className="m-b-15">
-                <Checkbox id="uppercase" label="Include Uppercase Letters"/>
-            </div>
-            <div className="m-b-15">
-                <Checkbox id="lowercase" label="Include Lowercase Letters"/>
-            </div>
-            <div className="m-b-15">
-                <Checkbox id="numbers" label="Include Numbers"/>
-            </div>
-            <div className="m-b-15">
-                <Checkbox id="symbols" label="Include Symbols"/>
-            </div>
-            <div className="m-t-15 m-b-15">
-                <ForcaDaSenha />
-            </div>
-            <div className="m-b-15">
-                <Button  text="Generate" onClick={gerarSenha}/>
-            </div>
-        </div>
+        </GeradorSenhaContext.Provider>
     )
 }
 
