0c3207c3822e9f102ce6fa956f228233d5398f4c
maicondguerian <maicondguerian@gmail.com>
2023-10-28 03:01:29 -0300

    arrumado duplicacao de componente

--- src/components/InputPassword/InputPassword.js
+++ src/components/InputPassword/InputPassword.js
@@ -13,26 +13,18 @@ const InputPassword = () => {
         <>
             <div className={styles.containerInputPassword}>
                 <input className={styles.inputPassword} placeholder={context.inputPasswordState.placeholder} value={context.inputPasswordState.password} disabled />
-                <FontAwesomeIcon
-                    icon={faCopy}
-                    onClick={() => context.copy(context.inputPasswordState.password)}
-                    data-tooltip-id='copied'
-                />
-                {iscopied && (
-                    <Tooltip
-                        id="copied"
-                        content="copiado"
-                        place='top'
-                        events={['click']}
-
-                    />
-                )}
-            </div>
-            <div className={styles.containerInputPassword}>
-                <input className={styles.inputPassword} placeholder={context.inputPasswordState.placeholder} value={context.inputPasswordState.password} disabled />
                 <div>
-                    <FontAwesomeIcon icon={faCopy} onClick={() => context.copy(context.inputPasswordState.password)} />
                     <FontAwesomeIcon icon={faRotate} onClick={() => context.copy(context.inputPasswordState.password)} />
+                    <FontAwesomeIcon icon={faCopy} onClick={() => context.copy(context.inputPasswordState.password)} data-tooltip-id='copied' />
+                    {iscopied && (
+                        <Tooltip
+                            id="copied"
+                            content="copiado"
+                            place='top'
+                            events={['click']}
+
+                        />
+                    )}
                 </div>
             </div>
         </>
